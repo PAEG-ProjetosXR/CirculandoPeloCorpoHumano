68f7d3f31379cfb62d89dda1c0300328614d2599
kaiogkiel <kaio.g.kiel@unesp.br>
2025-05-14 19:29:10 -0300

    feat: Versão inicial do Ranking + pequenas alterações visuais

--- Assets/Scripts/Logica Controladora/SceneGameOverManager.cs
+++ Assets/Scripts/Logica Controladora/SceneGameOverManager.cs
@@ -14,7 +14,10 @@ public class SceneGameOverManager : MonoBehaviour
     //-----------------------------
     [SerializeField] private TextMeshProUGUI _timerGameText;  // Exibe o tempo total de jogo
     [SerializeField] private TextMeshProUGUI _scoreGameText;  // Exibe a pontuação final
-
+    [SerializeField] private TextMeshProUGUI _result1Text;    // Exibe o resultado do 1º colocado
+    [SerializeField] private TextMeshProUGUI _result2Text;    // Exibe o resultado do 2º colocado
+    [SerializeField] private TextMeshProUGUI _result3Text;    // Exibe o resultado do 3º colocado
+    [SerializeField] private TextMeshProUGUI _result4Text;    // Exibe o resultado do 4º colocado
     //-----------------------------
     // Métodos Unity
     //-----------------------------
@@ -35,6 +38,7 @@ public class SceneGameOverManager : MonoBehaviour
     /// Atualiza os textos de tempo e pontuação com dados do GameManager.
     /// Logs erros se componentes essenciais não forem encontrados.
     /// </summary>
+    
     private void UpdateGameOverUI() 
     {
         if (GameManager.Instance == null)
@@ -44,17 +48,46 @@ public class SceneGameOverManager : MonoBehaviour
         }
 
         int totalScore = GameManager.Instance.GetPontos();
+        float totalTimeSpent = GameManager.Instance.GetTotalTime();
+
+        // Atualiza os resultados dos jogadores
+        if (_result1Text != null) 
+        {
+            //Aqui os jogadores ainda estão com nomes genéricos
+            _result1Text.text = $"1º: Jogador 1 / Tempo: {Mathf.FloorToInt(totalTimeSpent)} / Pontuação: {totalScore}";
+            _result2Text.text = $"2º: Jogador 2 / Tempo: {GetRandomTime()} / Pontuação: {GetRandomScore(totalScore)}";
+            _result3Text.text = $"3º: Jogador 3 / Tempo: {GetRandomTime()} / Pontuação: {GetRandomScore(totalScore)}";
+            _result4Text.text = $"4º: Jogador 4 / Tempo: {GetRandomTime()} / Pontuação: {GetRandomScore(totalScore)}";
+        }
+        else 
+        {
+            Debug.LogError("Result1Text não atribuído no Inspector!");
+        }
+
         _scoreGameText.text = $"PONTOS: {totalScore}";
 
         if (_timerGameText != null) 
-    {
-            float totalTimeSpent = GameManager.Instance.GetTotalTime();
+        {
             _timerGameText.text = $"TEMPO: {Mathf.FloorToInt(totalTimeSpent)}";
         }
         else 
         {
             Debug.LogError("TimerGameText não atribuído no Inspector!");
         }
+    }
+
+    private int GetRandomTime() 
+    {
+        return 600 + Random.Range(0, 1320);  // Simula um tempo aleatório
+    }
+
+    private int GetRandomScore(int totalScore) 
+    {
+        if (totalScore < 10) //Jogador não acertou nenhuma questão
+        {
+            return 0;
+        }
+        return (Random.Range(0, totalScore - 10) / 10) * 10;  // Simula uma pontuação aleatória
     }
 
     //-----------------------------
