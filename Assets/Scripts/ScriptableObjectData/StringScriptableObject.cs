e2581b0705c21341910eaed6ee1773cfae1cb671
Bruno <brunoconcli@gmail.com>
2025-05-31 15:33:17 -0300

    feat: Adicionados novos Assets de persistência de dados entre cenas

--- Assets/Scripts/ScriptableObjectData/StringScriptableObject.cs
+++ Assets/Scripts/ScriptableObjectData/StringScriptableObject.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
 [CreateAssetMenu]
-public class FloatScriptableObject : ScriptableObject
+public class StringScriptableObject : ScriptableObject
 {
   [SerializeField]
-  private float _value;
-  public float Value
+  private string _value;
+  public string Value
   {
     get { return _value; }
     set { _value = value; }
